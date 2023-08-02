f296442df03433812bd1df63f2e4e46b45b7d923
jatin1213 <jatin@shyplite.com>
2023-08-02 18:54:41 +0530

    Design changes added on login page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Login from './assets/components/Login.js';
+import Login from './components/Login.js';
 import './assets/styles/index.scss'
 
 function App() {
---
+++ src/layout/loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <div className='fs16 text-red-400 mt-20'>loading ...</div>
+  )
+}
+
+export default loading

ff5e68ca965b41b4ef974a6b13c050825c222f93
jatin1213 <jatin@shyplite.com>
2023-08-02 12:54:20 +0530

    Boilerplate code added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./assets/styles/index.css";
+import './assets/styles/index.scss'
 
 function App() {
-  return <></>;
+  return (
+    <>
+
+    <h1 className="bg">
+      Hello world!
+    </h1>
+</>
+  );
 }
 
 export default App;

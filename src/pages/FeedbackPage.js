ca57014fc47d6986b162ef9390560f9b7e7d1e47
jatin1213 <jatin@shyplite.com>
2023-08-08 17:35:47 +0530

    radio buttons added.

--- src/pages/FeedbackPage.js
+++ src/pages/FeedbackPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import MultiselectAtom from '../components/atom/Multiselect.js';
+import RadioButton from '../components/atom/RadioSelect.js';
 
 function FeedbackPage() {
-  return (
-    <div>
-        <MultiselectAtom />
-    </div>
-  )
+	return (
+		<div>
+			<MultiselectAtom />
+			<RadioButton />
+		</div>
+	)
 }
 
-export default FeedbackPage
+export default FeedbackPage;

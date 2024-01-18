8cd7894a3b0afc4b22fce705df3efa8a039a64c2
thecryptoeditor <jatin.aggarwal.15csc@bml.edu.in>
2024-01-19 00:51:38 +0530

    repo cleaned up for spoofmusica

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ClipPreview from "./pages/ClipPreviewPage.js";
-import Login from "./pages/LoginPage.js";
-import Review from "./components/Review.js";
 import "./assets/styles/index.scss";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route 
-          path="/login" 
-          element = {
-            <Login />
-          } 
-        />
-        <Route 
-          path="/clip" 
-          element = { 
-            <ClipPreview />
-          } 
-        />
-        <Route 
-          path="/feedback" 
-          element = { 
-            <Review />
-          } 
-        />
-        <Route 
-          path="/" 
-          element = { 
-            <Login /> 
-          } 
-        />
-      </Routes>
-    </BrowserRouter>
+    <></>
   );
 }
